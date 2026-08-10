299c9c1ba0d6b069468460235a8e52d3a4d01ab6
diwakrmatsa <21r21a3333@mlrinstitutions.ac.in>
2026-08-10 19:35:56 +0530

    fix: correct GitHub Pages url/organizationName to match actual account (21r21a33333)

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,14 +15,14 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://diwakrmatsa.github.io',
+  url: 'https://21r21a33333.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/system-design-course/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'diwakrmatsa', // Usually your GitHub org/user name.
+  organizationName: '21r21a33333', // Usually your GitHub org/user name.
   projectName: 'system-design-course', // Usually your repo name.
 
   onBrokenLinks: 'throw',
